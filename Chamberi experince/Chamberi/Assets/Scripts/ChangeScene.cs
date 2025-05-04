562fd2dc09cda92bed5939cd988398e91392639c
Paulalemany <palemany@ucm.es>
2025-05-04 22:29:24 +0200

    Cambia de escena

--- Chamberi experince/Chamberi/Assets/Scripts/ChangeScene.cs
+++ Chamberi experince/Chamberi/Assets/Scripts/ChangeScene.cs
@@ -7,6 +7,7 @@ public class ChangeScene : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        
         SceneManager.LoadScene(1);  //Cambia a la pantalla de fin
     }
 }
